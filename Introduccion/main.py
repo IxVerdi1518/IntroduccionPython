e516826bef071461ac7de0583d1ca8bef22eba4f
IxVerdi1518 <estebanfe400@gmail.com>
2023-05-25 20:15:54 -0500

    Carga de proyecto

---
+++ Introduccion/main.py
@@ -0,0 +1,11 @@
+print("Este es mi saludos desde Python")
+x = 10
+y = 12
+print (x+y)
+print("Este es el ejercicio del modulo 1: ")
+_nombre = "Esteban Flores"
+_telefono=9995394073
+_correo="esteban@gmail.com"
+print(_nombre)
+print(_telefono)
+print(_correo)
